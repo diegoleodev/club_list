4d61420acb1e41e5dea26122cafd89c81bd8a945
Diego <diegoleonardo912@gmail.com>
2024-11-11 20:12:46 -0300

    Project created

---
+++ src/main/java/com/diegodev/clublist/ClublistApplication.java
@@ -0,0 +1,13 @@
+package com.diegodev.clublist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClublistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClublistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegodev/clublist/ClublistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegodev.clublist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClublistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
